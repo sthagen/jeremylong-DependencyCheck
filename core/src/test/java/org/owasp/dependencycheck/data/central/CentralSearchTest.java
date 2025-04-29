c96e318f7b970941e4548e7daa9e8f25c7f4a7f5
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-04-29 16:38:53 +0200

    Migrate tests to JUnit5 (fixes)
    * Make CentralSearchTest more stable

--- core/src/test/java/org/owasp/dependencycheck/data/central/CentralSearchTest.java
+++ core/src/test/java/org/owasp/dependencycheck/data/central/CentralSearchTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.owasp.dependencycheck.BaseTest;
 import org.owasp.dependencycheck.data.nexus.MavenArtifact;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
@@ -46,13 +48,16 @@ class CentralSearchTest extends BaseTest {
     @Test
     void testValidSha1() throws Exception {
         try {
-        List<MavenArtifact> ma = searcher.searchSha1("9977a8d04e75609cf01badc4eb6a9c7198c4c5ea");
-        assertEquals("org.apache.maven.plugins", ma.get(0).getGroupId(), "Incorrect group");
-        assertEquals("maven-compiler-plugin", ma.get(0).getArtifactId(), "Incorrect artifact");
-        assertEquals("3.1", ma.get(0).getVersion(), "Incorrect version");
-                } catch (IOException ex) {
-            //we hit a failure state on the CI
+            List<MavenArtifact> ma = searcher.searchSha1("9977a8d04e75609cf01badc4eb6a9c7198c4c5ea");
+            assertEquals("org.apache.maven.plugins", ma.get(0).getGroupId(), "Incorrect group");
+            assertEquals("maven-compiler-plugin", ma.get(0).getArtifactId(), "Incorrect artifact");
+            assertEquals("3.1", ma.get(0).getVersion(), "Incorrect version");
+        } catch (IOException ex) {
+            // abort if we hit a failure state on the CI
             assumeFalse(StringUtils.contains(ex.getMessage(), "Could not connect to MavenCentral"));
+            assumeFalse(ex.getMessage().matches("^https://.+ - Server status: \\d{3} - Server reason: .+$"));
+
+            // otherwise fail the test
             throw ex;
         }
     }
@@ -62,15 +67,15 @@ class CentralSearchTest extends BaseTest {
     // test it anyway
     @Test
     void testMissingSha1() {
-        assertThrows(IOException.class, () -> {
-            try {
-                searcher.searchSha1("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-            } catch (IOException ex) {
-                //we hit a failure state on the CI
-                assumeFalse(StringUtils.contains(ex.getMessage(), "Could not connect to MavenCentral"));
-                throw ex;
-            }
-        });
+        IOException ex = assertThrows(IOException.class, () -> searcher.searchSha1("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
+
+        //abort if we hit a failure state on the CI
+        assumeFalse(StringUtils.contains(ex.getMessage(), "Could not connect to MavenCentral"));
+        assumeFalse(ex.getMessage().matches("^https://.+ - Server status: \\d{3} - Server reason: .+$"));
+
+        // otherwise assert that the exception is a FileNotFoundException
+        assertInstanceOf(FileNotFoundException.class, ex);
+        assertEquals("Artifact not found in Central", ex.getMessage());
     }
 
     // This test should give us multiple results back from Central
@@ -80,8 +85,11 @@ class CentralSearchTest extends BaseTest {
             List<MavenArtifact> ma = searcher.searchSha1("94A9CE681A42D0352B3AD22659F67835E560D107");
             assertTrue(ma.size() > 1);
         } catch (IOException ex) {
-            //we hit a failure state on the CI
+            // abort if we hit a failure state on the CI
             assumeFalse(StringUtils.contains(ex.getMessage(), "Could not connect to MavenCentral"));
+            assumeFalse(ex.getMessage().matches("^https://.+ - Server status: \\d{3} - Server reason: .+$"));
+
+            // otherwise fail the test
             throw ex;
         }
     }
