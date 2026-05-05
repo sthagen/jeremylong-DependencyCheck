fd97985d069e9a3cc24cbc72bbafb8c64418f868
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-05-05 16:36:12 +0800

    build: make CHANGELOG easier to generate; with entries sorted by conventional commit priority
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- list-changes.sh
+++ list-changes.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-set -euo pipefail
-##https://blogs.sap.com/2018/06/22/generating-release-notes-from-git-commit-messages-using-basic-shell-commands-gitgrep/
-git --no-pager log "$(git describe --tags --abbrev=0)..HEAD" --pretty=format:" - %s" \
-  | grep -v ' - Bump' \
-  | sed -E 's/#([0-9]+)/[#\1](https:\/\/github.com\/dependency-check\/DependencyCheck\/pull\/\1)/g'
+set -uo pipefail
+function commits_for() {
+  ##https://blogs.sap.com/2018/06/22/generating-release-notes-from-git-commit-messages-using-basic-shell-commands-gitgrep/
+  git --no-pager log "$(git describe --tags --abbrev=0)..HEAD" --pretty=format:" - %s" \
+    | grep -E -v '^ - build: (prepare|release)' \
+    | grep -E "^ - $1" \
+    | sed -E 's/#([0-9]+)/[#\1](https:\/\/github.com\/dependency-check\/DependencyCheck\/pull\/\1)/g' \
+    | sort
+}
+
+printf "\n## [Version X.Y.Z](https://github.com/dependency-check/DependencyCheck/releases/tag/vX.Y.Z) (yyyy-MM-dd)\n\n"
+for prefix in "feat" "fix" "perf" "docs" "chore" "refactor" "test" "ci" "build"; do
+  commits_for "$prefix"
+done
+printf "\nSee the full listing of [changes](https://github.com/dependency-check/DependencyCheck/milestone/CHANGEME?closed=1)\n\n"
